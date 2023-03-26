f70dd6cb9508a7ecbf2e41f6738640617f246007
Mario Minches <javier_minches@hotmail.com>
2023-03-25 23:21:30 -0500

    MarioMinches[Create]Se creo el controlador Pagos CRUD

---
+++ API/Controllers/PagosController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Models;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PagosController : ControllerBase
+    {
+        private readonly Aerolinea_DesarrolloContext _context;
+
+        public PagosController(Aerolinea_DesarrolloContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pagos
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pagos>>> GetPagos()
+        {
+            return Ok(await _context.Pagos.ToListAsync());
+        }
+
+        // GET: api/Pagos/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pagos>> GetPagos(int id)
+        {   
+            var pagos = await _context.Pagos.FindAsync(id);
+
+            if (pagos == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pagos);
+        }
+
+        // PUT: api/Pagos/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPagos(int id, Pagos pagos)
+        {
+            if (id != pagos.idPago)
+            {
+                return BadRequest("El id no coincide");
+            }
+
+            _context.Entry(pagos).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PagosExists(id))
+                {
+                    return NotFound("No se Encontro el pago");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(pagos);
+        }
+
+        // POST: api/Pagos
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Pagos>> PostPagos(Pagos pagos)
+        {
+          
+            _context.Pagos.Add(pagos);
+            await _context.SaveChangesAsync();
+
+            return Ok(pagos);
+        }
+
+        // DELETE: api/Pagos/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePagos(int id)
+        { 
+            var pagos = await _context.Pagos.FindAsync(id);
+            if (pagos == null)
+            {
+                return NotFound("No se encontro el pago");
+            }
+
+            _context.Pagos.Remove(pagos);
+            await _context.SaveChangesAsync();
+
+            return Ok("Pago eliminado");
+        }
+
+        private bool PagosExists(int id)
+        {
+            return (_context.Pagos?.Any(e => e.idPago == id)).GetValueOrDefault();
+        }
+    }
+}
