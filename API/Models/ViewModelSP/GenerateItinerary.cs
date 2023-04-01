4d04e788d8c8bb1db4946a691f2432b822875619
rvasquez <rodrigovasquez225@gmail.com>
2023-03-31 23:15:55 -0600

    rvasquez [Create] Creacion de ViewModels

---
+++ API/Models/ViewModelSP/GenerateItinerary.cs
@@ -0,0 +1,14 @@
+﻿namespace API.Models.ViewModelSP
+{
+    public class GenerateItinerary
+    {
+        public string? DiaSemana { get; set; }
+        public string? HoraSalida { get; set; }
+        public string? HoraLlegada { get; set; }
+        public string? CIUDAD_ORIGEN { get; set; }
+        public string? CIUDAD_DESTINO { get; set; }
+        public string? PAIS_ORIGEN { get; set; }
+        public string? PAIS_DESTINO { get; set; }
+        public string? ESCALAS_JSON { get; set; }
+    }
+}
