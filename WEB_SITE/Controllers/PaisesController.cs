3cd221a7ed80501e848eeecdcf7c484d2c3ca446
amer7361 <erikitagonzalez2001@gmail.com>
2023-04-30 21:14:05 -0600

    egonzalez[create]crear paises

--- WEB_SITE/Controllers/PaisesController.cs
+++ WEB_SITE/Controllers/PaisesController.cs
@@ -25,6 +25,23 @@ namespace WEB_SITE.Controllers
         {
             return View();
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Paises model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var client = _http.CreateClient("Base");
+            var response = await client.PostAsJsonAsync("Paises", model);
+            if (!response.IsSuccessStatusCode)
+            {
+                return Redirect("Error");
+            }
+            TempData["CreatePais"] = "Pais creado correctamente";
+            return RedirectToAction("Index");
+        }
         public IActionResult Modify()
         {
             return View();
